8bf6149761d03bd8e300319791b8c34a0bff94ae
AR-Night <ricciardo.andrea@gmail.com>
2026-07-25 06:21:53 +0200

    Refresh cache for EVP Recorder v2.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='myspiritmic-v2.1';
-const CORE=['./','./index.html','./style.css?v=2.1','./app.js?v=2.1','./manifest.webmanifest'];
+const CACHE='myspiritmic-v2.3';
+const CORE=['./','./index.html','./style.css?v=23','./app-v23.js?v=23','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
